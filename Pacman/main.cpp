f5fa01dfcef3ebd64852c089a393fe897c0fc51f
PowerOleg <geraskin.oleg@mail.ru>
2025-01-22 17:20:27 +0300

    nice bug

--- Pacman/main.cpp
+++ Pacman/main.cpp
@@ -1,5 +1,5 @@
 #include <SFML/Graphics.hpp>
-//5:07
+//8:43
 using namespace sf;
 
 const int H = 21;
@@ -15,9 +15,9 @@ String tile_map[H] = {
     "A AA AAA A AAA AA A",//4
     "A    A   A   A    A",//5
     "AAAA AAA A AAA AAAA",//6
-    "BBBA A       A ABBB",//7
+    "BBBA A   DG  A ABBB",//7
     "AAAA A AAAAA A AAAA",//8
-    "BBBB B BAAAB B BBBB",//9
+    "BBBB B ABBBA B BBBB",//9
     "AAAA A AAAAA A AAAA",//10
     "BBBA A       A ABBB",//11
     "AAAA A AAAAA A AAAA",//12
@@ -50,61 +50,68 @@ public:
     int new_y = 0;
     Direction rotate = Direction::RIGHT;//in the begining here was 1==right, 2==left, 3==up, 4=down
 
-    void Update()
-    {
-        frame += 0.01;
-        if (frame > 5)
-        {
-            frame -= 5;
-        }
+	void Update()
+	{
+		frame += 0.01;
+		if (frame > 5)
+		{
+			frame -= 5;
+		}
 
-        switch (rotate)
-        {
-        case Direction::LEFT:
-            if (tile_map[y][new_x - 1] != 'A')
-            {
-                new_x--;
-            }
-            break;
-        case Direction::RIGHT:
-            if (tile_map[y][new_x + 1] != 'A')
-            {
-                new_x++;
-            }
-            break;
-        case Direction::UP:
-            if (tile_map[new_y - 1][x] != 'A')
-            {
-                new_y--;
-            }
-            break;
-        case Direction::DOWN:
-            if (tile_map[new_y + 1][x] != 'A')
-            {
-                new_y++;
-            }
-            break;
-        }
+		delay++;
+		if (delay >= 300)
+		{
+			switch (rotate)
+			{
+			case Direction::LEFT:
+				if (tile_map[y][new_x - 1] != 'A')
+				{
+					new_x--;
+				}
+				break;
+			case Direction::RIGHT:
+				if (tile_map[y][new_x + 1] != 'A')
+				{
+					new_x++;
+				}
+				break;
+			case Direction::UP:
+				if (tile_map[new_y - 1][x] != 'A')
+				{
+					new_y--;
+				}
+				break;
+			case Direction::DOWN:
+				if (tile_map[new_y + 1][x] != 'A')
+				{
+					new_y++;
+				}
+				break;
+			}
+			delay = 0;
+		}
 
-        
-        if (tile_map[new_y][new_x] == ' ')//the game engine to move the player through dots
-        {
-            tile_map[y][x] = 'B';
-            tile_map[new_y][new_x] = 'C';
+		if (tile_map[new_y][new_x] == ' ')//the game engine to move the player through dots
+		{
+			tile_map[y][x] = 'B';
+			tile_map[new_y][new_x] = 'C';
 
-            x = new_x;
-            y = new_y;
-            score++;
-        } 
-        else if (tile_map[new_y][new_x] == 'B')//the game engine to move the player through the field
-        {
-            tile_map[y][x] = 'B';
-            tile_map[new_y][new_x] = 'C';
+			x = new_x;
+			y = new_y;
+			score++;
+		}
+		else if (tile_map[new_y][new_x] == 'B')//the game engine to move the player through the field
+		{
+			tile_map[y][x] = 'B';
+			tile_map[new_y][new_x] = 'C';
 
-            x = new_x;
-            y = new_y;
-        }
-    };
+			x = new_x;
+			y = new_y;
+		}
+
+        PlayerOutOfBounds();
+
+	};
 
    float GetFrame()
    {
@@ -128,10 +135,157 @@ public:
 private:
     float frame = 0;
     int score = 0;
+    int delay = 0;
+
+    void PlayerOutOfBounds()
+    {
+        if (new_y == 9 && (new_x == 0 || new_x == 18))
+        {
+            if (new_x == 0)
+            {
+                new_x = 17;
+            }
+            else
+            {
+                new_x = 1;
+            }
+
+            tile_map[y][x] = 'B';
+            tile_map[new_y][new_x] = 'C';
+
+            x = new_x;
+            y = new_y;
+        }
+    }
 };
 
 
-void RepaintGameField(RenderWindow &window, Sprite &plat, Player &player)
+class Enemy {
+public:
+    int x = 9;
+    int y = 7;
+    int new_x = x;
+    int new_y = y;
+    int rotate = 1;
+    //Direction rotate = Direction::RIGHT;//in the begining here was 1==right, 2==left, 3==up, 4=down
+    
+    Enemy(char name_, int x_, int y_) : name{ name_ }, x{x_}, y{y_}
+    {}
+
+    void Update()
+    {
+        //frame += 0.01;
+        //if (frame > 5)
+        // {
+        //    frame -= 5;
+        //}
+
+        delay++;
+        if (delay >= 300)
+        {
+            this->rotate = (rand() % 4) + 1;
+
+            switch (rotate)
+            {
+            case 1:
+                if (tile_map[y][new_x - 1] != 'A')
+                {
+                    new_x--;
+                }
+                break;
+            case 2:
+                if (tile_map[y][new_x + 1] != 'A')
+                {
+                    new_x++;
+                }
+                break;
+            case 3:
+                if (tile_map[new_y - 1][x] != 'A')
+                {
+                    new_y--;
+                }
+                break;
+            case 4:
+                if (tile_map[new_y + 1][x] != 'A')
+                {
+                    new_y++;
+                }
+                break;
+            }
+            delay = 0;
+        }
+
+        if (tile_map[new_y][new_x] == ' ')//the game engine to move the player through dots
+        {
+            tile_map[y][x] = ' ';
+            tile_map[new_y][new_x] = name;
+
+            x = new_x;
+            y = new_y;
+        }
+        else if (tile_map[new_y][new_x] == 'B')//the game engine to move the player through the field
+        {
+            tile_map[y][x] = 'B';
+            tile_map[new_y][new_x] = name;
+
+            x = new_x;
+            y = new_y;
+        }
+
+        PlayerOutOfBounds();
+
+    };
+
+    float GetFrame()
+    {
+        return this->frame;
+    }
+    int GetRotation()
+    {
+        return this->rotate;
+        /*switch (this->rotate)
+        {
+        case Direction::LEFT:
+            return 2;
+        case Direction::RIGHT:
+            return 1;
+        case Direction::UP:
+            return 3;
+        case Direction::DOWN:
+            return 4;
+        }*/
+    }
+
+private:
+    float frame = 0;
+    int score = 0;
+    int delay = 0;
+    char name;
+
+    void PlayerOutOfBounds()
+    {
+        if (new_y == 9 && (new_x == 0 || new_x == 18))
+        {
+            if (new_x == 0)
+            {
+                new_x = 17;
+            }
+            else
+            {
+                new_x = 1;
+            }
+
+            tile_map[y][x] = 'B';
+            tile_map[new_y][new_x] = name;
+
+            x = new_x;
+            y = new_y;
+        }
+    }
+};
+
+
+void RepaintGameField(RenderWindow &window, Sprite &plat, Player &player, Enemy &enemy)
 {
     for (size_t i = 0; i < H; i++)
     {
@@ -149,6 +303,10 @@ void RepaintGameField(RenderWindow &window, Sprite &plat, Player &player)
             {
                 plat.setTextureRect(IntRect(ts, 0, ts, ts));
             }
+            if (tile_map[i][j] == 'D')
+            {
+                plat.setTextureRect(IntRect(ts * 5, ts * enemy.GetRotation(), ts, ts));//arguments: Left, Top, Width, Height 
+            }
             if (tile_map[i][j] == 'B')
             {
                 continue;
@@ -165,12 +323,14 @@ void RepaintGameField(RenderWindow &window, Sprite &plat, Player &player)
 
 int main(int argc, char** argv)
 {
-    RenderWindow window(VideoMode(W * ts, H * ts), "Maze!");
+    RenderWindow window(VideoMode(W * ts, H * ts), "Hey Polly=)");
     Texture t;
     t.loadFromFile("C:\\cpp\\games\\visual_studio\\Pacman\\Pacman\\pacmansprites2.png");
     Sprite plat(t);
 
     Player player;
+    Enemy enemy1('D', 9, 7);
+    Enemy enemy2('G', 10, 7);
 
     while (window.isOpen())
     {
@@ -206,9 +366,11 @@ int main(int argc, char** argv)
             }
         }
         player.Update();
+        enemy1.Update();
+        enemy2.Update();
         window.clear(Color::Black);
         
-        RepaintGameField(window, plat, player);
+        RepaintGameField(window, plat, player, enemy1);
     }
     return 0;
 }
